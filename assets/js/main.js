d070ee1d77da7898701c37a7e23fdb36aeaeea11
Alessandro <marzucoalessandro1@gmail.com>
2020-11-23 14:54:26 +0100

    funzione genera numeri casuali tra 1 e 100

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,8 @@
 // Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
 // La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+
+//1 utilizziamo una funzione per generare numeri casuali tra 1 e 100.
+function generatoreNumeri(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+};
