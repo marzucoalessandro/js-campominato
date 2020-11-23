36462ba55842ffbaf1efdf6fddadffd3794de2a4
Alessandro <marzucoalessandro1@gmail.com>
2020-11-23 15:21:15 +0100

    ciclo per reiterare la funzione 16 vlt

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,17 @@
 // La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 
-//1 utilizziamo una funzione per generare numeri casuali tra 1 e 100.
-function generatoreNumeri(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) ) + min;
-};
+
+//1 creiamo una funzione che generi numeri casuali;
+ function generatoreNumeri() {
+   return Math.floor(Math.random() * 100) + 1;
+ };
+
+//2 creaimo un array vuoto per contenere i numeri che genera il computer;
+var contenitoreComp = [];
+
+//3 adesso creiamo un ciclo che esegua la nostra funzione 16 volte e la testiamo sulla console;
+for (i = 0; i < 16; i++) {
+  console.log(generatoreNumeri(i));
+  
+}
