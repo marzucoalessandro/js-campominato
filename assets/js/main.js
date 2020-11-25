86f3df23708c8ccb72e82e813a5cfd8df55c58eb
Alessandro <marzucoalessandro1@gmail.com>
2020-11-25 16:05:42 +0100

    inserimento di uno switch nel programma

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,12 +6,54 @@
 // La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 
+//aggiunta per provare il bonus//
+//proviamo a costruire uno switch per le tre differenti situazioni di gioco.
+//chiediamo un numero tra uno e tre per settare il livello di difficoltà;
+//un alert indicherà il livello scelto;
+
+var condizione = Number(prompt("inserisci un numero tra 1 e 3 per selezionare il livello di gioco ...."));
+
+if (condizione == 1) {
+  alert("hai deciso di giocare al livello facile");
+} else if (condizione == 2) {
+  alert("hai deciso di giocare al livello medio");
+} else {
+  alert("hai deciso di giocare al livello difficile");
+};
+
+//determiniamo quali sono le variabili in gioco che dovremo cambiare a seconda del livello.
+//per prima cosa nella ns funzione generatrice di numeri il 100 non è più un valore fisso ma diventa una variabile numerica.
+var counter;
+
+
+
+
+//a questo punto il nostro switch prenderà il numero scelto e setterà le condizioni nel gioco;
+switch (condizione) {
+  case 1:
+  counter = 50;
+
+  break;
+  case 2:
+  counter = 80;
+
+  break;
+  case 3:
+  counter = 100;
+
+  break;
+  default: 3
+}
+
+//dobbiamo anche cambiare il numero di iterazioni che farà il nostro ciclo:
+var iter = counter - 16;
+
 
 
 
 //1 creiamo una funzione che generi numeri casuali;
  function generatoreNumeri() {
-   return Math.floor(Math.random() * 100) + 1;
+   return Math.floor(Math.random() * counter) + 1;
  };
 
 
@@ -38,9 +80,9 @@ var numeriUtente = [];
 
 //creiamo un ciclo per chiedere al giocatore di inserire 100-16 numeri:
 //all'interno creiamo un if con le varie condizioni del gioco;
-for (var i = 0; i < 84; i++) {
+for (var i = 0; i < iter; i++) {
 
-    var domanda = Number(prompt("inserisci un numero da 0 a 100 senza ripeterlo...."));
+    var domanda = Number(prompt("inserisci un numero da 0 a " + counter + " senza ripeterlo...."));
 
 
     if (compare(contenitoreComp, domanda) == true) {
@@ -49,7 +91,7 @@ for (var i = 0; i < 84; i++) {
     } else if (compare(numeriUtente, domanda) == true) {
       console.log("hai ripetuto il numero " + domanda);
       i--;
-    } else if (domanda < 1 || domanda > 100){
+    } else if (domanda < 1 || domanda > counter){
       console.log("hai inserito un numero sbagliato");
       i--;
     } else {
@@ -61,9 +103,9 @@ for (var i = 0; i < 84; i++) {
 //adesso dobbiamo tirare i risultati della ns partita;
 
 //creiamo una nuova condizione:
-  if (i > 84) {
+  if (i > iter) {
     console.log("hai vinto!");
-  } else if (i >= 0 || i <= 84) {
+  } else if (i >= 0 || i <= iter) {
     console.log( "hai tirato " + i + " volte");
   }
 
